2860e52449018500867035b02fc2f9f4063f3dc3
unnatijajal <88263364+unnatijajal@users.noreply.github.com>
2022-02-02 18:58:43 +0530

    Update urls.py

--- dictionary/urls.py
+++ dictionary/urls.py
@@ -5,6 +5,5 @@ import dictionary.views
 urlpatterns =[
 
     path('', dictionary.views.home, name="home"),
-    # path('form', dictionary.views.change),
-    path('/', dictionary.views.talk, name="btn")
+   
 ]
